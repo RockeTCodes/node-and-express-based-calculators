55844ce8c01e8c667f5adc612cf6ebf0a48bc94b
RockeTCodes <74184474+RockeTCodes@users.noreply.github.com>
2022-11-10 19:03:06 +0530

    Update calculator.js

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -35,6 +35,24 @@ app.post("/",function(req,res){
   res.send("The result after calculation is "+ result);
 });
 
+app.get("/bmicalculator",function(req,res){
+  res.sendFile(__dirname+"/bmiCalculator.html");
+});
+
+app.post("/bmicalculator",function(req,res){
+
+
+var weight = parseFloat(req.body.weight);
+var height = parseFloat(req.body.height);
+
+var bmi = weight/(height*height);
+
+res.send("Your BMI Is: " + bmi);
+
+
+
+});
+
 app.listen(3000,function(){
   console.log("server started");
 });
